5a268ee0ba38d0203e54c626c4e66fe2dea966f7
talawson05 <talawson05@gmail.com>
2019-09-16 15:11:41 +0100

    added browser specific directory

--- test/test_bbc_weather.py
+++ test/test_bbc_weather.py
@@ -6,6 +6,6 @@ from test.bbc_weather.bbc_weather_home_page import BbcWeatherHomePage
 
 def test_open_bbc_weather():
     #path based on executing from root
-    driver = InstantiateDriver().chrome("./resources/drivers/chromedriver.exe")
+    driver = InstantiateDriver().chrome("./resources/drivers/chrome/chromedriver.exe")
     home_page = BbcWeatherHomePage(driver, Logger().init_logger())
     home_page.open()
